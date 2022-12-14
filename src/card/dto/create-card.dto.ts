7ab4fce0c29764ae5173b501dbeba19a12993d31
Mahmudov Mirabdulloh <mmirabdullo1999@gmail.com>
2022-12-15 04:55:41 +0500

    Add card model and dto

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,1 +1,23 @@
-export class CreateCardDto {}
+import { IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateCardDto {
+  @IsNotEmpty()
+  @IsNumber()
+  ticket_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  customer_id: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  createdAt: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
+  finishedAt: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  status_id: number;
+}
--- src/card/entities/card.entity.ts
+++ src/card/entities/card.entity.ts
@@ -1,1 +1,42 @@
-export class Card {}
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+@Table({tableName: 'card', timestamps: false, freezeTableName: true})
+export class Card extends Model<Card>{
+    @Column({
+        type: DataType.INTEGER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true
+    })
+    id: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    ticket_id: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    customer_id: number
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false
+    })
+    createdAt: Date
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false
+    })
+    finishedAt: Date
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    status_id: number
+}
