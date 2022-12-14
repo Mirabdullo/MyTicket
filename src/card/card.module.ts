d24bcdc9ed94649bf517eb3fcc9ade0cfcb8e9c1
Mahmudov Mirabdulloh <mmirabdullo1999@gmail.com>
2022-12-15 04:59:48 +0500

    Add card service

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Card } from './entities/card.entity';
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([Card])
+  ],
   controllers: [CardController],
   providers: [CardService]
 })
--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { Card } from './entities/card.entity';
 
 @Injectable()
 export class CardService {
+  constructor(
+    @InjectModel(Card) private cardRepository: typeof Card
+  ){}
   create(createCardDto: CreateCardDto) {
-    return 'This action adds a new card';
+    return this.cardRepository.create(createCardDto)
   }
 
   findAll() {
-    return `This action returns all card`;
+    return this.cardRepository.findAll({include: {all: true}})
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} card`;
+    return this.cardRepository.findByPk(id, {include: {all: true}})
   }
 
   update(id: number, updateCardDto: UpdateCardDto) {
-    return `This action updates a #${id} card`;
+    return this.cardRepository.update(updateCardDto, {where: {id}, returning: true})
   }
 
   remove(id: number) {
-    return `This action removes a #${id} card`;
+    return this.cardRepository.destroy({where: {id}})
   }
 }
